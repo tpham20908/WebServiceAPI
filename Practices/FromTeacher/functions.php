c1b27f2689342aa85e028c1ea6aeba98941ec5f8
Tung Pham <tungint@gmail.com>
2018-07-13 11:43:57 -0400

    jul13 in class

--- Practices/FromTeacher/functions.php
+++ Practices/FromTeacher/functions.php
@@ -13,9 +13,32 @@ function create_links($arg=""){
 
 //responses
 function response($data, $status=200){
+  if (isset($_GET["format"])) {
+    if ($_GET["format"] == "xml") {
+      response_as_xml();
+    } elseif ($_GET["format"] == "json") {
+      response_as_json();
+    } else {
+      $link = $data["link"];
+      response_as_json(
+        array("Error"=>"Unknown format", "link"=>$link),
+        400
+      );
+    }
+  }
+}
+
+function response_as_json($data, $status) {
   header("Content-Type:application/json");
-  header("HTTP/1.1 " . $status);
+  header("HTTP/1.1 " .$status);
   echo json_encode($data);
+}
+
+function response_as_xml($data, $status) {
+  header("Content-Type: application/xml");
+  $xml_data = new SimpleXMLElement("<?xml version='1.0'?><data></data>");
+  array_to_xml($data, $xml_data);
+  print $xml_data->asXML();
 }
 
  ?>
